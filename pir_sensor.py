508876e1868a35c70eb5de2bd3438207a5ac266a
Yann Hervy <yann.hervy@unident.se>
2025-03-30 21:41:01 +0200

    pir

--- pir_sensor.py
+++ pir_sensor.py
@@ -83,12 +83,13 @@ def monitor_pir_sensor():
         while True:
             if monitoring:
                 current_state = GPIO.input(PIR_PIN)  # Read the current state of the PIR sensor
+                logger.info(f"Current state: {current_state}, Last state: {last_state}")
                 if current_state and not last_state:  # Motion detected (state changed from OFF to ON)
                     logger.info("Motion detected!")
                     payload = create_payload(source="pir-sensor", event="MOTION_DETECTED")
                     publish_payload(Topics.PIR_MOTION_DETECTED.value, payload)
                 last_state = current_state  # Update the last state
-            time.sleep(0.1)  # Polling interval
+            time.sleep(1)  # Polling interval
     except KeyboardInterrupt:
         logger.info("Stopping PIR sensor monitoring...")
     finally:
