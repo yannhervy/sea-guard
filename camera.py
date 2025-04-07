976bb4da868120b92f55dbcaab5489ce2a4a83f1
Yann Hervy <yann.hervy@unident.se>
2025-04-07 14:09:09 +0200

    Skip emoji

--- camera.py
+++ camera.py
@@ -62,18 +62,17 @@ def take_picture():
         subprocess.run(command, check=True)
         logger.info(f"Picture taken and saved to {picture_path}")
 
-        # Add overlay text with a black background, using Noto Color Emoji
+        # Add overlay text with a black background
         text_overlay = f"SEAHUT57 {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
         subprocess.Popen([
             "convert",
             str(picture_path),
-            "-font", "Noto-Color-Emoji",          # <-- Använd vår emoji-vänliga font
             "-pointsize", "32",
             "-fill", "white",
             "-undercolor", "black",
             "-gravity", "SouthWest",
             "-annotate", "+10+40",
-            text_overlay + " 📸",  # Lägg till emoji
+            text_overlay,
             str(picture_path)
         ])
         logger.info(f"Started overlay in background for {picture_path}")
